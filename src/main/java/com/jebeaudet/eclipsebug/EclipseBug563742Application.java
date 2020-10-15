6df547b87e82d102c5b9ac3cf29d34fa1141dd9c
Jacques-Etienne Beaudet <jebeaudet@coveo.com>
2020-10-15 16:06:46 -0400

    Added sample project

---
+++ src/main/java/com/jebeaudet/eclipsebug/EclipseBug563742Application.java
@@ -0,0 +1,9 @@
+package com.jebeaudet.eclipsebug;
+
+public class EclipseBug563742Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello slow world!");
+    }
+}
